1e5b11a804a7e842c7751d20903ccc3a6126a26d
pot11 <persistpot@gmail.com>
2024-02-21 14:11:58 +0800

    【设计模式】单例模式

---
+++ practice/src/main/java/p1/SingletonForHungry.java
@@ -0,0 +1,22 @@
+package p1;
+
+/**
+ * @Description: 饿汉式（立即加载）
+ * @Author zhengqiang
+ * @Date 2024/2/20
+ */
+public class SingletonForHungry {
+
+    //私有化构造函数（保证通过反射自己创建，不会被其他类new创建）
+    private SingletonForHungry() {
+    }
+
+    //将自己实例化对象设置成一个属性，并用static.final修饰
+    private static final SingletonForHungry instance = new SingletonForHungry();
+
+    //static静态方法返回改实例
+    public static SingletonForHungry getInstance(){
+        return instance;
+    }
+
+}
--- practice/src/main/java/p1/SingletonForlazy.java
+++ practice/src/main/java/p1/SingletonForlazy.java
@@ -1,10 +1,12 @@
 package p1;
 //懒汉式 双重锁定
 public class SingletonForlazy {
+    //将自己实例化对象设置成一个属性，并用static修饰
     private volatile static SingletonForlazy instance;
 
     public SingletonForlazy getInstance(){
         if (instance != null){
+            //synchronized保证线程安全
             synchronized (SingletonForlazy.class){
                 instance = new SingletonForlazy();
             }
